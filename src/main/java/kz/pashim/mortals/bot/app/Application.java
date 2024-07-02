0f60918e38b70a067e969df21d3a87b9ffddd06e
isanayatov <sanayatov.islam@gmail.com>
2024-07-02 13:29:19 +0500

    hello world

--- src/main/java/kz/pashim/mortals/bot/app/Application.java
+++ src/main/java/kz/pashim/mortals/bot/app/Application.java
@@ -1,8 +1,12 @@
 package kz.pashim.mortals.bot.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
-    public static void main(String args[]) {
-        System.out.println("hello");
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/kz/pashim/mortals/bot/app/controller/TestController.java
@@ -0,0 +1,13 @@
+package kz.pashim.mortals.bot.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello world!";
+    }
+}
