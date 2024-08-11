39213b4974a80fbca944abb298330c6baa1e3c56
isanayatov <sanayatov.islam@gmail.com>
2024-08-11 22:12:56 +0500

    psql enum

--- src/main/java/kz/pashim/mortals/bot/app/model/UserEntity.java
+++ src/main/java/kz/pashim/mortals/bot/app/model/UserEntity.java
@@ -1,9 +1,12 @@
 package kz.pashim.mortals.bot.app.model;
 
+import io.hypersistence.utils.hibernate.type.basic.PostgreSQLEnumType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -17,11 +20,16 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@TypeDef(
+        name = "pgsql_enum",
+        typeClass = PostgreSQLEnumType.class
+)
 public class UserEntity extends BaseEntity {
     private String nickname;
     private String title;
     private String sourceUserId;
     @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
     private UserRole role;
     @ManyToOne
     private GroupEntity group;
