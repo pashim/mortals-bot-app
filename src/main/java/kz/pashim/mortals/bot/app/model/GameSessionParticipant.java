8870080cdcd440e738a377a26285d11836478705
isanayatov <sanayatov.islam@gmail.com>
2024-08-13 14:09:03 +0500

    test

--- src/main/java/kz/pashim/mortals/bot/app/model/GameSessionParticipant.java
+++ src/main/java/kz/pashim/mortals/bot/app/model/GameSessionParticipant.java
@@ -2,14 +2,11 @@ package kz.pashim.mortals.bot.app.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -24,7 +21,7 @@ import javax.persistence.Table;
 @Builder(toBuilder = true)
 @ToString(exclude = {"gameSession"})
 public class GameSessionParticipant extends BaseEntity {
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "game_session_id")
     private GameSessionEntity gameSession;
     @ManyToOne
